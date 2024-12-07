9c1ff66e78b9ef4dcdb3259f82fea74a2cfd2ba3
OSwego <syava23223@gmail.com>
2024-12-07 17:35:51 +0200

    Update GetData method

--- src/app/DataRepository.java
+++ src/app/DataRepository.java
@@ -6,10 +6,6 @@ import java.util.List;
 public class DataRepository {
     // Метод повертає список імен
     public List<String> getData() {
-        List<String> list = new ArrayList<>();
-        list = List.of("Alice", "Bob", "Lucy", "Denis", "Tom");
-
-
-        return list;
+        return new ArrayList<>(List.of("Alice", "Bob", "Lucy", "Denis", "Tom"));
     }
 }
